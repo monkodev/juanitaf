e1793e1c9e9babcec235c456f9507a65a3a0b509
MonKoDev Technologies <gmalugani@gmail.com>
2021-10-27 13:12:19 -0300

    27_10

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import React from 'react';
 
 import Links from './components/Links'
+import LinkForm from './components/LinkForm'
 
 function App() {
   return (
-    <Links/>
+
     <div className="App">
       
       <h2> Login Inicial </h2>
       <img  src={profileImage} alt="profile-image" />
+      <>
+        <LinkForm />
+        <Links />
+      </>
     </div>
 
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-  import firebase from 'firebase/app'
-  import 'firebase/firestore'
+
+  import firebase from 'firebase/compat/app';
+  //import "firebase/firestore";
+  //import firebaseConfig from './firebaseConfig'
 
   // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyC2eFjbmk4dwCyfd1XsHSLjjDi39x6diHA",
-    authDomain: "juanitaf-a4c25.firebaseapp.com",
-    projectId: "juanitaf-a4c25",
-    storageBucket: "juanitaf-a4c25.appspot.com",
-    messagingSenderId: "299627340461",
-    appId: "1:299627340461:web:be37b92ae2cb430f129c24"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyC2eFjbmk4dwCyfd1XsHSLjjDi39x6diHA",
+  authDomain: "juanitaf-a4c25.firebaseapp.com",
+  projectId: "juanitaf-a4c25",
+  storageBucket: "juanitaf-a4c25.appspot.com",
+  messagingSenderId: "299627340461",
+  appId: "1:299627340461:web:be37b92ae2cb430f129c24"
+};
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  const fb = firebase.initializeApp(firebaseConfig);
+  const db = fb.firestore();
 
-  export const db = app.firestore();
+export default db
+
+
+  //export const db = app.firestore();
